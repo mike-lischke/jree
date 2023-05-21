8205836b958746db7dbbd1bed9eddd14401df0e5
Mike Lischke <mike.lischke@oracle.com>
2023-05-21 14:33:15 +0200

    Fixing a file renaming issue.

--- tests/java/util/JDK Collections.spec.ts
+++ tests/java/util/JDK Collections.spec.ts
@@ -4,7 +4,7 @@
  */
 
 import { ListDefaults } from "../../jdk/java/util/List/ListDefaults";
-import { TestNG } from "../../org/testng/TestNG";
+import { TestNG } from "../../org/testng/TestNG1";
 
 /** Executes the ListDefault JDK tests. */
 describe("Run JDK Collections Tests", () => {
